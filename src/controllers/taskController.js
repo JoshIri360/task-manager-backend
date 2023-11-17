5005edf8ebf5da97c987e298a7ca08c93e635e2a
Joshua Aideloje <aidelojejoshua@gmail.com>
2023-11-17 14:08:56 +0100

    Optimized routes and sorting

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -11,14 +11,22 @@ class TaskController {
     });
   };
 
-  getTasks = (req, res) => {
-    Task.find({}, (err, tasks) => {
-      if (err) {
-        return res.status(500).json({ message: err.message });
-      }
-      return res.status(200).json(tasks);
-    });
-  };
+  getTasks(req, res) {
+    const sortField = req.query.sort;
+    const allowedFields = ["dueDate", "priority"];
+    if (sortField && !allowedFields.includes(sortField)) {
+      return res.status(400).json({ message: "Invalid sort field" });
+    }
+
+    Task.find({})
+      .sort({ [sortField]: -1 })
+      .exec((err, tasks) => {
+        if (err) {
+          return res.status(500).json({ message: err.message });
+        }
+        return res.status(200).json(tasks);
+      });
+  }
 
   getTask = (req, res) => {
     Task.findById(req.params.id, (err, task) => {
@@ -47,71 +55,52 @@ class TaskController {
       if (err) {
         return res.status(500).json({ message: err.message });
       }
-      return res.status(200).json(task);
+      return res.status(200).json({ message: "Task deleted" });
     });
   };
 
   completeTask = (req, res) => {
     const { id } = req.params;
-    Task.findByIdAndUpdate(
-      id,
-      { completed: true },
-      { new: true },
-      (err, task) => {
+    Task.findById(id, (err, task) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
+      }
+      if (!task) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+      if (task.completed) {
+        return res.status(400).json({ message: "Task already completed" });
+      }
+      task.completed = true;
+      task.save((err, updatedTask) => {
         if (err) {
           return res.status(500).json({ message: err.message });
         }
-        return res.status(200).json(task);
-      }
-    );
+        return res.status(200).json(updatedTask);
+      });
+    });
   };
 
   unCompleteTask = (req, res) => {
     const { id } = req.params;
-    Task.findByIdAndUpdate(
-      id,
-      { completed: false },
-      { new: true },
-      (err, task) => {
-        if (err) {
-          return res.status(500).json({ message: err.message });
-        }
-        return res.status(200).json(task);
+    Task.findById(id, (err, task) => {
+      if (err) {
+        return res.status(500).json({ message: err.message });
       }
-    );
-  };
-
-  sortByDueDate = (req, res) => {
-    Task.find({})
-      .sort({ dueDate: 1 })
-      .exec((err, tasks) => {
+      if (!task) {
+        return res.status(404).json({ message: "Task not found" });
+      }
+      if (!task.completed) {
+        return res.status(400).json({ message: "Task already uncompleted" });
+      }
+      task.completed = false;
+      task.save((err, updatedTask) => {
         if (err) {
           return res.status(500).json({ message: err.message });
         }
-        return res.status(200).json(tasks);
+        return res.status(200).json(updatedTask);
       });
-  };
-
-  sortByPriority = (req, res) => {
-    Task.find({})
-      .sort({ priority: -1 })
-      .exec((err, tasks) => {
-        if (err) {
-          return res.status(500).json({ message: err.message });
-        }
-        return res.status(200).json(tasks);
-      });
-  };
-
-  sortByDateCreated = (req, res) => {
-    Task.find({})
-      .sort({ createdAt: -1 })
-      .exec((err, tasks) => {
-        if (err) {
-          return res.status(500).json({ message: err.message });
-        }
-        return res.status(200).json(tasks);
-      });
+    });
   };
 
   getCompletedTasks = (req, res) => {
--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -4,40 +4,28 @@ const TaskController = require("../controllers/taskController");
 const router = express.Router();
 const taskController = new TaskController();
 
-router.post("/tasks", (req, res) => {
-  taskController.createTask(req, res);
-});
+router
+  .route("/tasks")
+  .post((req, res) => taskController.createTask(req, res))
+  .get((req, res) => taskController.getTasks(req, res));
 
-router.get("/tasks", (req, res) => {
-  taskController.getTasks(req, res);
-});
+router
+  .route("/tasks/completed")
+  .get((req, res) => taskController.getCompletedTasks(req, res));
 
-router.get("/tasks/completed", (req, res) => {
-  taskController.getCompletedTasks(req, res);
-});
+router
+  .route("/tasks/:id")
+  .get((req, res) => taskController.getTask(req, res))
+  .put((req, res) => taskController.updateTask(req, res))
+  .delete((req, res) => taskController.deleteTask(req, res));
 
-router.get("/tasks/:id", (req, res) => {
-  taskController.getTask(req, res);
-});
+router
+  .route("/tasks/:id/complete")
+  .put((req, res) => taskController.completeTask(req, res));
 
-router.put("/tasks/:id", (req, res) => {
-  taskController.updateTask(req, res);
-});
-
-router.delete("/tasks/:id", (req, res) => {
-  taskController.deleteTask(req, res);
-});
-
-router.put("/tasks/:id/complete", (req, res) => {
-  taskController.completeTask(req, res);
-});
-
-router.put("/tasks/:id/uncomplete", (req, res) => {
-  taskController.unCompleteTask(req, res);
-});
-
-router.get("/tasks/:id/uncomplete", (req, res) => {
-  taskController.getUnCompletedTasks(req, res);
-});
+router
+  .route("/tasks/:id/uncomplete")
+  .put((req, res) => taskController.unCompleteTask(req, res))
+  .get((req, res) => taskController.getUnCompletedTasks(req, res));
 
 module.exports = router;
